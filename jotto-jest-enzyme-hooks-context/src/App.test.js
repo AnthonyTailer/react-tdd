f5fe66629e25899b9e595ada56ea81ae5a50d015
Anthony Almeida <anthony.almeida@chipinside.com.br>
2020-06-13 14:04:23 -0300

    Fiest functional component Test

--- jotto-jest-enzyme-hooks-context/src/App.test.js
+++ jotto-jest-enzyme-hooks-context/src/App.test.js
@@ -1,9 +1,18 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { shallow } from 'enzyme'
+import { findByTestAttr } from '../test/testUtils'
+import App from './App'
+
+/**
+ * Setup function for app component
+ * @return {ShallowWrapper}
+ */
+const setup = () => {
+  return shallow(<App />)
+}
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const wrapper = setup()
+  const component = findByTestAttr(wrapper, 'component-app')
+  expect(component.length).toBe(1);
 });
