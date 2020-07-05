0e4eee6b927ad29434888290766f684c32dd6df4
Anthony Almeida <anthony.almeida@chipinside.com.br>
2020-07-05 11:55:12 -0300

    Add successContext.js and successContext.test.js

---
+++ jotto-jest-enzyme-hooks-context/src/contexts/successContext.test.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { shallow, mount } from 'enzyme'
+
+import successContext from './successContext'
+
+// a functional component tha calls useSuccess for our tests
+const FunctionalComponent = () => {
+  successContext.useSuccess()
+  return <div />
+}
+
+test('useSuccess throws error when not wrapped in SuccessProvider', () => {
+  expect(() => {
+    shallow(<FunctionalComponent />)
+  }).toThrow('useSuccess must be used within a SuccessProvider')
+})
+
+test('useSuccess does not throws error when wrapped in SuccessProvider', () => {
+  expect(() => {
+    mount(
+      <successContext.SuccessProvider>
+        <FunctionalComponent />
+      </successContext.SuccessProvider>
+    )
+  }).not.toThrow('useSuccess must be used within a SuccessProvider')
+})
+
